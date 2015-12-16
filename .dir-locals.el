335f2442afeacb3203a26bd2d8d862983dee9890
Etienne <etienne@ThinkPax>
2015-12-15 21:24:11 -0500

    chore: Update emacs configuration

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,3 @@
-((scss-mode . ((scss-compile-at-save . nil))))
+((scss-mode . ((scss-compile-at-save . nil)
+               (css-indent-offset . 2)
+               (scss-sass-command . "scss --style expanded"))))
